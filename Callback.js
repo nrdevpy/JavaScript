9dfad41a5d7f62719874263e58157f731b59b808
Nelson Rojas <nelson_rojas_janda@hotmail.com>
2022-03-02 12:41:52 +0100

    Callback

---
+++ Callback.js
@@ -0,0 +1,44 @@
+/* El termino callback hace referencia al uso de una funcion
+dentro de otra, introduciendolo mediante los parametros */
+
+function myFunction1 (){
+    console.log('Funcion 1');
+}
+
+function myFunction2 (){
+    console.log('Funcion 2');
+}
+
+// * Funcion Callback.
+function imprimir (msg){
+    console.log(msg);
+}
+
+function sumar (op1, op2, callbackFunction){
+    let res = op1 + op2;
+    callbackFunction(`Resultado: ${res}`);
+}
+// ! Testing callback
+// sumar (2, 8, imprimir);
+
+// * Llamadas asincronas con uso setTimeout.
+
+function myCallbackFunction(){
+    console.log('Saludo despues de 3 seg');
+}
+
+// ! Testing setTimeout
+/* setTimeout(myCallbackFunction, 3000);
+
+setTimeout(function(){console.log('Saludos en 3 segundos mas')}, 6000);
+
+setTimeout(() => console.log('Ultimo saludo en 3 segundos mas'), 9000); */
+
+// * Lammadas asincronas con uso setInterval.
+
+let reloj = () => {
+    let fecha = new Date();
+    console.log(`${fecha.getHours()}:${fecha.getMinutes()}:${fecha.getSeconds()}`)
+}
+
+// setInterval(reloj, 5000); // Se llama cada 5 segundos.
