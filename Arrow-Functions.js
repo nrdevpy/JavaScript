79a7cace52b804951eb608a061bdfa2a6a2a1292
Nelson Rojas <nelson_rojas_janda@hotmail.com>
2022-03-02 12:29:55 +0100

    Arrow Functions

---
+++ Arrow-Functions.js
@@ -0,0 +1,33 @@
+// * Funciones Normales
+
+/* function myFuncion(){
+    console.log('Saludos desde mi funcion.');
+}
+myFuncion(); */
+
+// * Arrow Functions
+// ! Con las Arrow Functions no se aplica el termino Hoisting.
+
+/* const myArrowFunction = () => {
+    console.log("Saludos desde Arrow Function.")
+}
+myArrowFunction(); */
+
+//* Arrow Functions en una linea de codigo.
+
+/* const myArrowFunction = () => console.log('Saludos desde Arrow Function en una linea de codigo.');
+myArrowFunction(); */
+
+// * Arrow Functions devolviendo valor.
+
+/* const saludar = () => {
+    return 'Arrow Function devolviendo valor';
+} */
+
+/* const saludar = () => 'Arrow Function devolviendo valor';
+console.log(saludar()); */
+
+// * Arrow Functions devolviendo objeto.
+
+/* const devuelveObjeto = () => ({nombre: 'Nelson', apellido: 'Rojas'});
+console.log(devuelveObjeto()); */
